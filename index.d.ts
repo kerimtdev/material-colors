ab417973790f4b4765d64b37ddb59087e94c1faf
Kerim Tuncer <kerimpsd@icloud.com>
2020-08-22 23:15:02 +0300

    First Commit

---
+++ index.d.ts
@@ -0,0 +1,41 @@
+import Amber from './colors/amber.json';
+import Blue from './colors/blue.json';
+import BlueGrey from './colors/blue-grey.json';
+import Brown from './colors/brown.json';
+import Cyan from './colors/cyan.json';
+import DeepOrange from './colors/deep-orange.json';
+import DeepPurple from './colors/deep-purple.json';
+import Green from './colors/green.json';
+import Grey from './colors/grey.json';
+import Indigo from './colors/indigo.json';
+import LightBlue from './colors/light-blue.json';
+import LightGreen from './colors/light-green.json';
+import Lime from './colors/lime.json';
+import Orange from './colors/orange.json';
+import Pink from './colors/pink.json';
+import Purple from './colors/purple.json';
+import Red from './colors/red.json';
+import Teal from './colors/teal.json';
+import Yellow from './colors/yellow.json';
+
+export {
+  Amber,
+  Blue,
+  BlueGrey,
+  Brown,
+  Cyan,
+  DeepOrange,
+  DeepPurple,
+  Green,
+  Grey,
+  Indigo,
+  LightBlue,
+  LightGreen,
+  Lime,
+  Orange,
+  Pink,
+  Purple,
+  Red,
+  Teal,
+  Yellow
+};
